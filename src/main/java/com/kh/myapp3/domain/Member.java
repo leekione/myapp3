c11e7b4e79c2d193d38ae2237984611c70e82033
leekione <byesg1127@naver.com>
2022-09-02 17:25:40 +0900

    20220902

--- src/main/java/com/kh/myapp3/domain/Member.java
+++ src/main/java/com/kh/myapp3/domain/Member.java
@@ -3,6 +3,8 @@ package com.kh.myapp3.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor //디폴트 생성자 만드는 어노테이션
 public class Member {
@@ -10,7 +12,8 @@ public class Member {
     private String email;       // 이메일  EMAIL	VARCHAR2(40 BYTE)	Yes		2
     private String pw;          // 비밀번호   PW	VARCHAR2(10 BYTE)	Yes		3
     private String nickname;    // 별칭   NICKNAME	VARCHAR2(30 BYTE)	Yes		4
-
+    private LocalDateTime cdate;
+    private LocalDateTime udate;
     public Member(String email, String pw, String nickname) {
         this.email = email;
         this.pw = pw;
