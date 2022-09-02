af803dea40578d5d5ebb2e9b2b4cd257746fddde
leekione <byesg1127@naver.com>
2022-09-02 17:22:59 +0900

    20220902

---
+++ src/main/java/com/kh/myapp3/domain/dao/MemberDAO.java
@@ -0,0 +1,48 @@
+package com.kh.myapp3.domain.dao;
+
+import com.kh.myapp3.domain.Member;
+
+import java.util.List;
+
+public interface MemberDAO {
+    /**
+     * 등록
+     * @param member 회원정보
+     * @return 가입건수
+     */
+    int insert(Member member);
+
+    /**
+     * 조회 by 회원 아이디
+     * @param memberId 회원아이디
+     * @return 회원정보
+     */
+    Member findById(Long memberId);
+
+    /**
+     * 수정
+     * @param memberId 아이디
+     * @param member 수정할 회원 정보
+     * @return 수정건수
+     */
+    int update(Long memberId, Member member);
+
+    /**
+     * 탈퇴
+     * @param memberId 회원아이디
+     * @return 삭제건수
+     */
+    int delete(Long memberId);
+
+    /**
+     * 전체목록
+     * @return 회원전체
+     */
+    List<Member> all();
+
+    /**
+     * 신규 회원아이디(내부관리용) 생성
+     * @return 회원아이디
+     */
+    Long generateMemberId();
+}
---
+++ src/main/java/com/kh/myapp3/domain/svc/MemberSVCImpl.java
@@ -0,0 +1,78 @@
+package com.kh.myapp3.domain.svc;
+
+import com.kh.myapp3.domain.Member;
+import com.kh.myapp3.domain.dao.MemberDAO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberSVCImpl implements MemberSVC{
+
+    private final MemberDAO memberDAO;
+    /**
+     * 등록
+     *
+     * @param member 회원정보
+     * @return 회원아이디
+     */
+    @Override
+    public Member insert(Member member) {
+
+        //회원아이디 생성
+        Long generateMemberId = memberDAO.generateMemberId();
+        member.setMemberId(generateMemberId);
+        memberDAO.insert(member);
+        return memberDAO.findById(generateMemberId);//dao와 svc 타입이 다름
+    }
+
+    /**
+     * 조회 by 회원 아이디
+     *
+     * @param memberId 회원아이디
+     * @return 회원정보
+     */
+    @Override
+    public Member findById(Long memberId) {
+        return memberDAO.findById(memberId);
+    }
+
+    /**
+     * 수정
+     *
+     * @param memberId 아이디
+     * @param member   수정할 회원 정보
+     * @return 수정건수
+     */
+    @Override
+    public void update(Long memberId, Member member) {
+        int cnt = memberDAO.update(memberId,member);
+        log.info("수정건수={}",cnt);
+    }
+
+    /**
+     * 탈퇴
+     *
+     * @param memberId 회원아이디
+     * @return 삭제건수
+     */
+    @Override
+    public void delete(Long memberId) {
+        int cnt = memberDAO.delete(memberId);
+        log.info("삭제건수={}", cnt);
+    }
+
+    /**
+     * 전체목록
+     *
+     * @return 회원전체
+     */
+    @Override
+    public List<Member> all() {
+        return memberDAO.all();
+    }
+}
